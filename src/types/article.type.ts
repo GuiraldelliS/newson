7fd9d8fc50604146d6d1a3b5f47ec17322ede281
GuiraldelliS <eduardoschelive@gmail.com>
2021-09-11 23:59:25 -0300

    Create types for Article and News

---
+++ src/types/article.type.ts
@@ -0,0 +1,9 @@
+export type ArticleType = {
+  id: string
+  title: string
+  summary: string
+  link: string
+  media: string
+  authors: string[]
+  publishedDate: Date
+}
---
+++ src/types/news.type.ts
@@ -0,0 +1,9 @@
+import { ArticleType } from './article.type'
+
+export type NewsType = {
+  page: number
+  pageSize: number
+  status: string
+  totalPages: number
+  articles: ArticleType[]
+}
